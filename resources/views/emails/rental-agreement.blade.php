1e88f0ce056b496e1c61ca59d5a6254903782235
Neguinho Admin <admin@neguinhomotors.co.uk>
2023-07-17 10:34:07 +0100

    changed link in rental-agreement email to web url

--- resources/views/emails/rental-agreement.blade.php
+++ resources/views/emails/rental-agreement.blade.php
@@ -10,7 +10,7 @@
     <p>{{ $body }}</p>
 
     <P>
-        You now need to upload your documents by clicking <a href="https://shopper.test/login">HERE</a>.<br>
+        You now need to upload your documents by clicking <a href="https://neguinhomotors.co.uk/login">HERE</a>.<br>
 
         Please use your email address as your username. Your password is provided below.<br>
         Password: Rental3786
